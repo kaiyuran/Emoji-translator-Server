af4c33a21be5cde89b804a2c0e722e7fa7df32b5
kaiyuran <73236245+kaiyuran@users.noreply.github.com>
2024-11-19 13:21:42 -0500

    Revert "change port"
    This reverts commit b8abf7f7d07fe43c6f02bd360f57904000b06eea.

--- app.py
+++ app.py
@@ -200,11 +200,15 @@ def emojify(message):
     return final
 
 
+
+
+
+
 app = Flask(__name__)
 CORS(app) 
 
-# if __name__ == "__main__":
-#     app.run(port=5500)  
+if __name__ == "__main__":
+    app.run(port=5500)  
 
 @app.route('/getemoji', methods=['GET'])
 # @cross_origin()
